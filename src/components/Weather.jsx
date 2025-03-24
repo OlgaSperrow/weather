36f859ff3bb17c61f9dab17d4128cf07795d4f94
OlgaSperrow <osperling17@gmail.com>
2025-03-24 11:42:28 +0100

    file Weather with &&

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 const Weather = ({weather, message}) => {
 
-    if(message){
-        return (<div className={'infoWeath'}>{message}</div>);
-    }
+
     return (
         <div className={'infoWeath'}>
-            <p>Location:  {weather.country}, {weather.city} </p>
-            <p>Temperatur: {weather.temp}°C</p>
-            <p>Preassure: {weather.pressure} pa</p>
-            <p>Sunset: { (new Date(weather.sunset*1000)).toLocaleTimeString()} </p>
+            {!message &&
+                <>
+                    <p>Location:  {weather.country}, {weather.city} </p>
+                    <p>Temperatur: {weather.temp}°C</p>
+                    <p>Preassure: {weather.pressure} pa</p>
+                    <p>Sunset: { (new Date(weather.sunset*1000)).toLocaleTimeString()} </p>
+                </>
+            }
+            {message}
         </div>
+
     );
 };
 
